5fd07bb73cddeea4ffcf8eb805174a2e82e897c1
CST <106782634+cstTaescol@users.noreply.github.com>
2022-06-02 20:03:32 -0500

    v3.3.2

--- SiPall/config/db.php
+++ SiPall/config/db.php
@@ -2,7 +2,7 @@
 class Database
 {
 	public static function connect(){
-		$db=new mysqli("127.0.0.1","taescol","cst","inventarios_taescol");
+		$db=new mysqli("127.0.0.1","taescol","inventarios","cst");
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
