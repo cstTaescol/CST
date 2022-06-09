16c16287418169ecad2a6730cbe063b9736c8003
CST <106782634+cstTaescol@users.noreply.github.com>
2022-06-09 18:22:57 -0500

    v3.3.3

--- SiPall/views/layouts/layout1/header.php
+++ SiPall/views/layouts/layout1/header.php
@@ -3,10 +3,10 @@
 <head>	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>	 	
+	<link rel="stylesheet" href="<?=base_Father?>js/linbsJqueryBootstrap/bootstrap.min.css">
+	<script src="<?=base_Father?>js/linbsJqueryBootstrap/jquery.min.js"></script>
+	<script src="<?=base_Father?>js/linbsJqueryBootstrap/popper.min.js"></script>
+	<script src="<?=base_Father?>js/linbsJqueryBootstrap/bootstrap.min.js"></script>	 	
 	<link href="<?=base_Father?>tema/estilo.css" rel="stylesheet" type="text/css" />
 	<link href="<?=base_Father?>js/estilo_menu.css" rel="stylesheet" type="text/css" />	
 	<!-- librería que declara la función Calendar Nativo del CST -->
